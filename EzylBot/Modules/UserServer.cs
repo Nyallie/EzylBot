0d54dc734e62f74a5da7df27b3bc219317e2297c
Théo LEBAILLIF <theolebaillif24@gmail.com>
2021-06-23 21:35:49 +0200

    Ajout d'une catégorie de command d'info de server ou d'user. - ajout d'une commande pour réccupérer la pdp

---
+++ EzylBot/Modules/UserServer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace EzylBot.Modules
+{
+    public class UserServer : ModuleBase<SocketCommandContext>
+    {
+        [Command("avatar")]
+        [Summary("Give link to the discord avatar")]
+        public async Task Avatar(IGuildUser user=null)
+        {
+            EmbedBuilder embedBuilder = new EmbedBuilder().WithCurrentTimestamp();
+            if (user == null)
+            {
+                string url = Context.User.GetAvatarUrl(ImageFormat.Auto,4096);
+                string name = Context.User.Username;
+                embedBuilder.WithTitle("name")
+                    .WithDescription($"[Link]({url})")
+                    .WithImageUrl(url);
+            }
+            else
+            {
+                string url = user.GetAvatarUrl(ImageFormat.Auto, 4096);
+                string name = user.Username;
+                embedBuilder.WithTitle("name")
+                    .WithDescription($"[Link]({url})")
+                    .WithImageUrl(url);
+            }
+            Embed embed = embedBuilder.Build();
+            await ReplyAsync(embed: embed);
+        }
+    }
+}
